ef51fd002472e69ced744fedc467bdea1026db38
TVau <tyler.r.vaughan@gmail.com>
2023-02-23 18:10:49 -0800

    Fix Culture issue with EN-GB

--- Calendar/MakeCalendarCsvs.ps1
+++ Calendar/MakeCalendarCsvs.ps1
@@ -10,7 +10,7 @@
         #DateTime is always in the current culture. I need to figure out another way to test besides just set-culture.
         #This brings up an issue with en-gb having the first week of the year start on the first four day week. It could cause issues with the first and last weeks of the year.
         #Just another reason America is the best.
-        $weeks = 1..($culture.Calendar.GetDaysInMonth($year, $cardinalMonth)) | ForEach-Object { [DateTime]::New($year,$cardinalMonth,$_, $culture.Calendar) } | Group-Object {$culture.Calendar.GetWeekOfYear($_, $culture.DateTimeFormat.CalendarWeekRule, $culture.DateTimeFormat.FirstDayOfWeek)}
+        $weeks = 1..($culture.Calendar.GetDaysInMonth($year, $cardinalMonth)) | ForEach-Object { [DateTime]::New($year,$cardinalMonth,$_, $culture.Calendar) } | Group-Object {$culture.Calendar.GetWeekOfYear($_, [System.Globalization.CalendarWeekRule]::FirstDay, $culture.DateTimeFormat.FirstDayOfWeek)}
         $weeks | Foreach-Object -Process {
             $week = "" | Select-Object $culture.DateTimeFormat.DayNames[$weekOrder]
             foreach($dayOfWeek in $culture.DateTimeFormat.DayNames)
